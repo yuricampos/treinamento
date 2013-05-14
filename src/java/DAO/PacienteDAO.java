e507492eaab8f93268746a469027b6bc9458182e
Yuri Campos <yuricampos@me.com>
2013-05-14 19:37:12 -0300

    paciente dao finalizado

--- src/java/DAO/PacienteDAO.java
+++ src/java/DAO/PacienteDAO.java
@@ -20,15 +20,15 @@ public class PacienteDAO implements IObjectDAO {
 
     private final String SQL_LOGIN = "SELECT p.id as p_id, p.nome as p_nome, p.cpf as p_cpf, p.rg as"
             + " p_rg, p.foto as p_foto, p.chave as p_chave, p.sexo as p_sexo, p.grupoSanguineo as p_grupoSanguineo, "
-            + "p.login as p_login, p.email as p_email FROM paciente AS p WHERE p.login = ? AND p.senha = ?";
+            + "p.login as p_login, p.email as p_email, p.dataNascimento as p_dataNascimento FROM paciente AS p WHERE p.login = ? AND p.senha = ?";
     private final String SQL_INSERT = "INSERT INTO paciente VALUES(DEFAULT,?,?,?,?,?,?,?,?,?,?,?);";
     private final String SQL_UPDATE = "UPDATE paciente SET senha = ?, email = ? WHERE id = ?";
-    private final String SQL_GET = "SELECT p.id as p_id, p.nome as p_nome, p.cpf as p_cpf, p.rg as"
+    private final String SQL_GET = "SELECT p.id as p_id, p.dataNascimento as p_dataNascimento , p.nome as p_nome, p.cpf as p_cpf, p.rg as"
             + " p_rg, p.foto as p_foto, p.chave as p_chave, p.sexo as p_sexo, p.grupoSanguineo as p_grupoSanguineo, "
             + "p.login as p_login, p.email as p_email FROM paciente AS p WHERE p.id = ?";
-    private final String SQL_GET_BY_KEY = "SELECT p.id as p_id, p.nome as p_nome, p.cpf as p_cpf, p.rg as"
+    private final String SQL_GET_BY_KEY = "SELECT p.id as p_id, p.dataNascimento as p_dataNascimento,  p.nome as p_nome, p.cpf as p_cpf, p.rg as"
             + " p_rg, p.foto as p_foto, p.chave as p_chave, p.sexo as p_sexo, p.grupoSanguineo as p_grupoSanguineo, "
-            + "p.login as p_login, p.email as p_email FROM paciente AS p WHERE p.chave = ?";
+            + "p.login as p_login, p.email as p_email FROM paciente AS p WHERE p.chave = ? and p.id = ?";
     private final String SQLUPDATEKEY = "UPDATE paciente SET chave = ? WHERE id = ?";
     private PreparedStatement ps;
     private ResultSet rs;
@@ -45,12 +45,6 @@ public class PacienteDAO implements IObjectDAO {
             armazenaChaves += letras.substring(index, index + 1);
         }
         return armazenaChaves;
-    }
-    
-    public void atualizarKey(Object input){
-        Paciente p = (Paciente) input;
-
-        
     }
 
     public Object verificaLogin(String usuario, String senha) throws SQLException {
@@ -75,10 +69,21 @@ public class PacienteDAO implements IObjectDAO {
         output.setRg(this.rs.getString("p_rg"));
         output.setSexo(this.rs.getString("p_sexo"));
         output.setChave(this.rs.getString("p_chave"));
-        
+        output.setDataNascimento(this.rs.getDate("p_dataNascimento"));
+        output.setGrupoSanguineo(this.rs.getString("p_grupoSanguineo"));
+        output.setLogin(this.rs.getString("p_login"));
         output.setFoto(this.rs.getString("p_foto"));
         output.setChave(this.rs.getString("p_rg"));
         return output;
+    }
+
+    public void atualizarKey(Object input) throws SQLException {
+        Paciente p = (Paciente) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQLUPDATEKEY);
+        this.ps.setString(1, gerarKey());
+        this.ps.setInt(2, p.getId());
+        this.ps.execute();
+
     }
 
     @Override
@@ -89,13 +94,25 @@ public class PacienteDAO implements IObjectDAO {
         this.ps.setString(2, p.getCpf());
         this.ps.setString(3, p.getRg());
         this.ps.setString(4, p.getFoto());
-        this.ps.setString(1, p.getNome());
+        this.ps.setString(5, gerarKey());
+        this.ps.setString(6, p.getSexo());
+        this.ps.setString(7, p.getGrupoSanguineo());
+        this.ps.setDate(8, p.getDataNascimento());
+        this.ps.setString(9, p.getLogin());
+        this.ps.setString(10, p.getSenha());
+        this.ps.setString(11, p.getEmail());
         this.ps.execute();
     }
 
     @Override
     public void atualizar(Object input) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Paciente p = (Paciente) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_UPDATE);
+        this.ps.setString(1, p.getSenha());
+        this.ps.setString(2, p.getEmail());
+        this.ps.setInt(3, p.getId());
+
+        this.ps.execute();
     }
 
     @Override
@@ -105,7 +122,22 @@ public class PacienteDAO implements IObjectDAO {
 
     @Override
     public Object recuperar(Object input) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Paciente p = (Paciente) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_GET);
+        this.ps.setInt(1, p.getId());
+        this.rs = this.ps.executeQuery();
+        rs.next();
+        return this.criarObjetoTemplate();
+    }
+
+    public Object recuperarKey(Object input) throws Exception {
+        Paciente p = (Paciente) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_GET_BY_KEY);
+        this.ps.setString(1, p.getChave());
+        this.ps.setInt(2, p.getId());
+        this.rs = this.ps.executeQuery();
+        rs.next();
+        return this.criarObjetoTemplate();
     }
 
     @Override
--- src/java/main.java
+++ src/java/main.java
@@ -1,8 +1,12 @@
 
 import DAO.MedicoDAO;
 import DAO.PacienteDAO;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import model.Medico;
+import model.Paciente;
 
 /*
  * To change this template, choose Tools | Templates
@@ -16,35 +20,64 @@ public class main {
 
     public static void main(String[] args) throws Exception {
         MedicoDAO mdao = new MedicoDAO();
-    //    int a = mdao.verificaLogin("123", "123");
-     //   System.out.println(a);
+        //    int a = mdao.verificaLogin("123", "123");
+        //   System.out.println(a);
         /**
-         * 
-         
-        Medico m = new Medico();
-        m.setNome("yuri");
-        m.setCrm("123456");
-        m.setEmail("a@a.com");
-        m.setSenha("111");
-        mdao.inserir(m);
-        ArrayList<Object> medicos = new ArrayList<Object>();
-        medicos = (ArrayList<Object>) mdao.buscar("f");
-        for(int i = 0 ; i < medicos.size() ; i++){
-            m = (Medico) medicos.get(i);
-            System.out.println(m.getNome());
-        } 
-        m = (Medico) mdao.recuperar("123");
-        System.out.println(m.getEmail());
-        m.setNome("yuri");
-        m.setCrm("123456");
-        m.setEmail("b@a.com");
-        m.setSenha("111");
-        mdao.atualizar(m);
-        System.out.println(m.getEmail());
-        * */
-        
+         *
+         *
+         * Medico m = new Medico(); m.setNome("yuri"); m.setCrm("123456");
+         * m.setEmail("a@a.com"); m.setSenha("111"); mdao.inserir(m);
+         * ArrayList<Object> medicos = new ArrayList<Object>(); medicos =
+         * (ArrayList<Object>) mdao.buscar("f"); for(int i = 0 ; i <
+         * medicos.size() ; i++){ m = (Medico) medicos.get(i);
+         * System.out.println(m.getNome()); } m = (Medico)
+         * mdao.recuperar("123"); System.out.println(m.getEmail());
+         * m.setNome("yuri"); m.setCrm("123456"); m.setEmail("b@a.com");
+         * m.setSenha("111"); mdao.atualizar(m);
+         * System.out.println(m.getEmail());
+        *
+         */
         PacienteDAO pdao = new PacienteDAO();
         String chave = pdao.gerarKey();
         System.out.println(chave);
+        Paciente p = new Paciente();
+        //  p.setNome("yuri");
+        //p.setCpf("123456");
+        //p.setSexo("M");
+        //p.setDataNascimento(coverteStringData("10/10/2010"));
+        // p.setLogin("1");
+        // p.setSenha("12345");
+        // p.setEmail("p@p.aaaacom");
+        //  pdao.inserir(p);
+        //p.setId(2);
+        //pdao.atualizar(p);
+        // p = (Paciente) pdao.verificaLogin("1", "12345");
+        Paciente p1 = new Paciente();
+        p1.setId(2);
+        p1.setChave("ZRSUBPLUL");
+        p = (Paciente) pdao.recuperarKey(p1);
+        System.out.println(p.getNome());
+
+    }
+
+    public static String converteData(Date d) {
+        if (d == null) {
+            return new String("");
+        }
+        return new SimpleDateFormat("dd/MM/yyyy").format(d);
+    }
+
+    public static Date coverteStringData(String data) {
+        if (data.equals("")) {
+            return null;
+        }
+
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return new java.sql.Date(formatador.parse(data).getTime());
+        } catch (ParseException ex) {
+            System.out.println("Erro conversao de Date!");
+            return null;
+        }
     }
 }
