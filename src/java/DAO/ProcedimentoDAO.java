b7944f82b3190229b688587bbf314abfb187da70
Yuri Campos <yuricampos@me.com>
2013-05-14 21:37:20 -0300

    procedimentoDAO finalizado

--- src/java/DAO/ProcedimentoDAO.java
+++ src/java/DAO/ProcedimentoDAO.java
@@ -17,15 +17,14 @@ import model.Procedimento;
  * @author yuricampos
  */
 public class ProcedimentoDAO implements IObjectDAO {
-    
-    
+
     private final String SQL_INSERT = "INSERT INTO procedimento VALUES(DEFAULT,?,?,?,?,?,?);";
     private final String SQL_UPDATE = "UPDATE procedimento SET nome = ?, descricao = ?, observacao = ?, "
             + "data = ? WHERE id = ?";
     private final String SQL_GETALL = "SELECT p.id as p_id, p.medico as p_medico, p.paciente as p_paciente,"
             + "p.nome as p_nome, p.descricao as p_descricao, p.observacao as p_observacao, p.data as p_data"
             + " FROM procedimento AS p WHERE p.paciente = ?";
-    private final String SQL_GET =  "SELECT p.id as p_id, p.medico as p_medico, p.paciente as p_paciente,"
+    private final String SQL_GET = "SELECT p.id as p_id, p.medico as p_medico, p.paciente as p_paciente,"
             + "p.nome as p_nome, p.descricao as p_descricao, p.observacao as p_observacao, p.data as p_data"
             + " FROM procedimento AS p WHERE p.id = ?";
     private PreparedStatement ps;
@@ -60,12 +59,28 @@ public class ProcedimentoDAO implements IObjectDAO {
 
     @Override
     public void inserir(Object input) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Procedimento p = (Procedimento) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_INSERT);
+        this.ps.setString(1, p.getMedico().getCrm());
+        this.ps.setInt(2, p.getPaciente().getId());
+        this.ps.setString(3, p.getNome());
+        this.ps.setString(4, p.getDescricao());
+        this.ps.setString(5, p.getObservacao());
+        this.ps.setDate(6, p.getData());
+        this.ps.execute();
     }
 
     @Override
     public void atualizar(Object input) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Procedimento p = (Procedimento) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_UPDATE);
+        this.ps.setString(1, p.getNome());
+        this.ps.setString(2, p.getDescricao());
+        this.ps.setString(3, p.getObservacao());
+        this.ps.setDate(4, p.getData());
+        this.ps.setInt(5, p.getId());
+        this.ps.execute();
+
     }
 
     @Override
@@ -75,7 +90,20 @@ public class ProcedimentoDAO implements IObjectDAO {
 
     @Override
     public Object recuperar(Object input) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Procedimento p = (Procedimento) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_GET);
+        this.ps.setInt(1, p.getId());
+        this.rs = this.ps.executeQuery();
+        rs.next();
+        return this.criarObjetoTemplate();
+    }
+
+    public Object getAll(Object input) throws Exception {
+        Procedimento p = (Procedimento) input;
+        this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_GETALL);
+        this.ps.setInt(1, p.getPaciente().getId());
+        this.rs = this.ps.executeQuery();
+        return this.buscarVariosObjetosTemplate();
     }
 
     @Override
@@ -87,5 +115,4 @@ public class ProcedimentoDAO implements IObjectDAO {
     public Object buscar(Object input) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
